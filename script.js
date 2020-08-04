76c0c69e38dd9fadc91f5aef2762683c3308be05
EvanCharles16 <evancharles1609@gmail.com>
2020-08-04 23:00:15 +0700

    add empty array for message that looping on HTML

--- script.js
+++ script.js
@@ -4,15 +4,24 @@ new Vue({
     playerHealth: 100,
     monsterHealth: 100,
     gameIsRunning: false,
+    turns: [],
   },
   methods: {
     startGame() {
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack() {
-      this.monsterHealth -= this.calculateDamage(3, 10);
+      var damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Player hits Monster for " + damage,
+      });
+
       if (this.checkWin()) {
         return; // Return for so if return true, i dont continue the code execution
       }
@@ -20,7 +29,14 @@ new Vue({
       this.monsterAttack();
     },
     specialAttack() {
-      this.monsterHealth -= this.calculateDamage(10, 20);
+      var damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Player hits Monster Hard for " + damage,
+      });
+
       if (this.checkWin()) {
         return; // Return for so if return true, i dont continue the code execution
       }
@@ -33,6 +49,11 @@ new Vue({
         this.playerHealth = 100;
       }
 
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Player heals for 10",
+      });
+
       this.monsterAttack();
     },
     giveup() {
@@ -44,7 +65,14 @@ new Vue({
       return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
     monsterAttack() {
-      this.playerHealth -= this.calculateDamage(5, 12);
+      var damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
+
+      this.turns.unshift({
+        isPlayer: false,
+        text: "Monster hits Player for " + damage,
+      });
+
       this.checkWin();
     },
     checkWin() {
